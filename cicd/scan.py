d85212e701b3458e236268228cb3503d94104923
Prabal Deb (prabdeb) <prabdeb@cisco.com>
2019-02-04 19:49:50 +0530

    Enable with Drone as a simple plugin to scan any secrets from BB Pull Request

--- cicd/scan.py
+++ cicd/scan.py
@@ -14,14 +14,18 @@ import shutil
 
 #################################################################################################
 class Scan:
-    def __init__(self, scmUrl, token, userName, project, repository, exclude, excludeRepositories, outputFile, verbose):
+    def __init__(self, scmUrl, token, userName, project, repository, exclude, excludeRepositories, outputFile, pullRequest, inputCommitDepth, entropy, verbose, exitWithError):
         self.scmUrl = scmUrl
         self.token = token
         self.userName = userName
         self.project = project
         self.repository= repository
         self.outputFile= outputFile
+        self.pullRequest = pullRequest
+        self.inputCommitDepth = inputCommitDepth
+        self.entropy = entropy
         self.verbose = verbose
+        self.exitWithError = exitWithError
         if exclude != None:
             self.exclude = exclude.split(",")
         else:
@@ -35,9 +39,17 @@ class Scan:
         else:
             logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
     def _getRepositories(self):
-        bitbucket = Bitbucket(self.scmUrl, self.token, self.userName, self.project, self.repository)
+        bitbucket = Bitbucket(self.scmUrl, self.token, self.userName, self.project, self.repository, self.pullRequest)
         return (bitbucket._getRepositories())
-    def _executeTruffleHog(self, repositories):
+    def _getPRCommitDepth(self):
+        if self.inputCommitDepth != None:
+            return self.inputCommitDepth
+        elif self.pullRequest != None:
+            bitbucket = Bitbucket(self.scmUrl, self.token, self.userName, self.project, self.repository, self.pullRequest)
+            return (bitbucket._getPRCommitDepth())
+        else:
+            return ""
+    def _executeTruffleHog(self, repositories, commitDepth):
         if os.path.exists("truffleHog"):
             shutil.rmtree("truffleHog", ignore_errors=True)
         os.makedirs("truffleHog")
@@ -48,8 +60,14 @@ class Scan:
             if repositoryName in self.excludeRepositories:
                 logging.info("["+str(index)+"]" + " Skipping execution for " + repositoryName)
                 continue
-            logging.info("["+str(index)+"]" + " Executing truffleHog for " + repositoryName + " ...")
-            os.system("truffleHog --regex --json " + repository + " > truffleHog/" + repositoryName + ".json")
+            if commitDepth != "":
+                logging.info("Setting Commit Depth to - " + commitDepth)
+                logging.info("Setting branch to - refs/pull-requests/" + self.pullRequest + "/merge")
+                logging.info("["+str(index+1)+"]" + " Executing truffleHog for " + repositoryName + " ...")
+                os.system("/usr/local/bin/trufflehog --regex --json --branch=refs/pull-requests/" + self.pullRequest + "/merge --max_depth=" + commitDepth + " --entropy=" + self.entropy + " " + repository + " > truffleHog/" + repositoryName + ".json")
+            else:
+                logging.info("["+str(index+1)+"]" + " Executing truffleHog for " + repositoryName + " ...")
+                os.system("/usr/local/bin/trufflehog --regex --json --entropy=" + self.entropy + " " + repository + " > truffleHog/" + repositoryName + ".json")
             truffleHogOutputFiles[repositoryName] = "truffleHog/" + repositoryName + ".json"
         return truffleHogOutputFiles
     def _readInputFile(self, inputJson):
@@ -103,12 +121,17 @@ class Scan:
                     logging.debug("      lastCommitHash - " + secretDetails["commitHash"])
                     logging.debug("      branch - " + secretDetails["branch"])
                     truffleHogExceldata.append([self.project, repositoryName, path, reason, secret, secretDetails["commitCount"], secretDetails["commitHash"], secretDetails["branch"]])
-        print("======================================================")
-        print("Total files having secret information - ", str(totalFiles))
-        print("Secret types:", ", ".join(reasonsFound))
-        print("\"git rm\" comand will not help to clean as it will present in history")
-        print("Follow to remove from history - https://help.github.com/articles/removing-sensitive-data-from-a-repository")
-        print("======================================================")
+        if totalFiles > 0:
+            print("======================================================")
+            print("Total files having secret information - ", str(totalFiles))
+            print("Secret types:", ", ".join(reasonsFound))
+            print("\"git rm\" comand will not help to clean as it will present in history")
+            print("Follow to remove from history - https://help.github.com/articles/removing-sensitive-data-from-a-repository")
+            print("======================================================")
+        else:
+            print("======================================================")
+            print("[PASSED] There are no files having secrets")
+            print("======================================================")
         return truffleHogExceldata
     def _excelReportInitiator(self):
         workbook = xlsxwriter.Workbook(self.outputFile.name, {'strings_to_formulas': False, 'strings_to_urls': False})
@@ -151,32 +174,39 @@ class Scan:
         return workbook
 
     def scan(self):
+        truffleHogExceldataSets = []
         if self.outputFile != None:
             workbook, worksheetAllDefect = self._excelReportInitiator()
-            truffleHogExceldataSets = []
         repositories = self._getRepositories()
-        truffleHogOutPutFiles = self._executeTruffleHog(repositories)
+        commitDepth = self._getPRCommitDepth()
+        truffleHogOutPutFiles = self._executeTruffleHog(repositories, commitDepth)
         for repositoryName in truffleHogOutPutFiles:
             truffleHogOutPutFile = truffleHogOutPutFiles[repositoryName]
             truffleHogResults = self._readInputFile(truffleHogOutPutFile)
             truffleHogResultFiltered = self._parseResult(truffleHogResults)
             truffleHogExceldata = self._print(repositoryName, truffleHogResultFiltered)
-            if self.outputFile != None:
-                truffleHogExceldataSets = truffleHogExceldataSets + truffleHogExceldata
+            truffleHogExceldataSets = truffleHogExceldataSets + truffleHogExceldata
         if self.outputFile != None:
             workbook = self._excelReport(workbook, worksheetAllDefect, truffleHogExceldataSets)
             workbook.close()
             logging.info("Report saved to - " + self.outputFile.name )
+        if len(truffleHogExceldataSets) > 0:
+            if self.exitWithError:
+                print("======================================================")
+                print("[FAILED] There are files having secrets")
+                print("======================================================")
+                sys.exit(2)
 
 #################################################################################################
 class Bitbucket:
-    def __init__(self, scmUrl, token, userName, project, repository):
+    def __init__(self, scmUrl, token, userName, project, repository, pullRequest):
         self.scmUrl = scmUrl
         self.token = token
         self.userName = userName
         self.apiUrl = scmUrl + "/rest/api/latest/"
         self.project = project
         self.repository = repository
+        self.pullRequest = pullRequest
     def _getRepositories(self):
         repositories = {}
         urlWithoutHttps = self.scmUrl
@@ -193,28 +223,39 @@ class Bitbucket:
         for entry in rawRepositoryData["values"]:
             repositories[entry["slug"]] = "https://"+self.userName+":"+self.token+"@"+urlWithoutHttps+"/scm/"+self.project+"/"+entry["slug"]+".git"
         return repositories
+    def _getPRCommitDepth(self):
+        logging.debug("Getting commit depth for - " + self.scmUrl + " for project - " + self.project + " Pull Request ID - " + self.pullRequest)
+        header = {'Authorization': 'Bearer ' + self.token}
+        response = requests.get(self.apiUrl + "projects/" + self.project + "/repos/" + self.repository + "/pull-requests/" + self.pullRequest + "/commits?limit=500", headers=header)
+        return str(response.json()["size"])
 
 #################################################################################################
 def main(args):
-    scmUrl, token, userName, project, repository, exclude, excludeRepositories, outputFile, verbose = get_args()
-    scan = Scan(scmUrl, token, userName, project, repository, exclude, excludeRepositories, outputFile, verbose)
+    scmUrl, token, userName, project, repository, exclude, excludeRepositories, outputFile, pullRequest, inputCommitDepth, entropy, verbose, exitWithError = get_args()
+    scan = Scan(scmUrl, token, userName, project, repository, exclude, excludeRepositories, outputFile, pullRequest, inputCommitDepth, entropy, verbose, exitWithError)
     scan.scan()
 
 #################################################################################################
 def get_args():
     '''This function parses and return arguments passed in'''
     parser = argparse.ArgumentParser(description='scan.py: Execute and parse report of truffleHog')
-    parser.add_argument('-s', '--scm', type=str, help='SCM parent URL for scaning', required=True)
-    parser.add_argument('-t', '--token', type=str, help='SCM OAuth Token', required=True)
-    parser.add_argument('-u', '--userName', type=str, help='SCM User Name', required=True)
-    parser.add_argument('-p', '--project', type=str, help='Project/Owner Name', required=True)
-    parser.add_argument('-r', '--repository', type=str, help='Repository Name, if not mentioned all repositories will be scanned', required=False)
-    parser.add_argument('-e', '--exclude', type=str, help='Excluded files patterns coma (,) separated', required=False)
-    parser.add_argument('-er', '--excludeRepositories', type=str, help='Excluded repositories coma (,) separated', required=False)
+    parser.add_argument('-s', '--scm', type=str, help='SCM parent URL for scaning (Mandatory) ENV: SCM_URL', required=False)
+    parser.add_argument('-t', '--token', type=str, help='SCM OAuth Token (Mandatory) ENV: BITBUCKET_LOGIN', required=False)
+    parser.add_argument('-u', '--userName', type=str, help='SCM User Name (Mandatory) ENV: DRONE_NETRC_USERNAME', required=False)
+    parser.add_argument('-p', '--project', type=str, help='Project/Owner Name (Mandatory) ENV: DRONE_REPO_OWNER', required=False)
+    parser.add_argument('-r', '--repository', type=str, help='Repository Name, if not mentioned all repositories will be scanned ENV: DRONE_REPO_NAME', required=False)
+    parser.add_argument('-e', '--exclude', type=str, help='Excluded files patterns coma (,) separated ENV: EXCLUDE_FILE_PATTERN', required=False)
+    parser.add_argument('-er', '--excludeRepositories', type=str, help='Excluded repositories coma (,) separated ENV: EXCLUDED_REPOSITORIES', required=False)
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), help='Store result to mentioned report file (Microsoft Excel)', required=False)
+    parser.add_argument('-pr', '--pullRequest', type=str, help='Enable diff based scan for Pull Request ENV: CI_PULL_REQUEST', required=False)
+    parser.add_argument('-cd', '--commitDepth', type=str, help='Enable diff based scan for commit depth ENV: DRONE_BUILD_EVENT', required=False)
+    parser.add_argument('-en', '--entropy',type=str, help='Enable (default) or disable entropy ENV: TRUFFLEHOG_ENTROPY', required=False)
     parser.add_argument('-v', '--verbose', help='Verbose output for debug', required=False, action='store_true')
-    parser.set_defaults(summary=True)
+    parser.add_argument('-ex', '--exit', help='Exit with error if any secrets found ENV: RAISE_ERROR', required=False, action='store_true')
+    parser.set_defaults(entropy="True")
     parser.set_defaults(verbose=False)
+    parser.set_defaults(exit=False)
+
     args = parser.parse_args()
     scmUrl = args.scm
     token = args.token
@@ -224,8 +265,52 @@ def get_args():
     exclude = args.exclude
     excludeRepositories = args.excludeRepositories
     outputFile = args.output
+    pullRequest = args.pullRequest
+    inputCommitDepth = args.commitDepth
+    entropy = args.entropy
     verbose = args.verbose
-    return scmUrl,token,userName,project,repository,exclude,excludeRepositories,outputFile,verbose
+    exitWithError = args.exit
+
+    # Take env variables for Drone
+    if scmUrl == None:
+        scmUrl = os.getenv('SCM_URL')
+    if exclude == None:
+        exclude == os.getenv('EXCLUDE_FILE_PATTERN')
+    if excludeRepositories == None:
+        excludeRepositories = os.getenv('EXCLUDED_REPOSITORIES')
+    if entropy == None:
+        entropy = os.getenv('TRUFFLEHOG_ENTROPY')
+    if token == None:
+        token = os.getenv('BITBUCKET_LOGIN')
+    if userName == None:
+        userName = os.getenv('DRONE_NETRC_USERNAME')
+    if project == None:
+        project = os.getenv('DRONE_REPO_OWNER')
+    if repository == None:
+        repository = os.getenv('DRONE_REPO_NAME')
+    if pullRequest == None:
+        pullRequest = os.getenv('CI_PULL_REQUEST')
+    if inputCommitDepth == None:
+        if os.getenv('DRONE_BUILD_EVENT') == "push":
+            inputCommitDepth = "1"
+    if exitWithError == False:
+        exitWithError = (os.getenv('RAISE_ERROR', default="False") == "True")
+    
+    # Validate mandatory parameters
+    if scmUrl == None:
+        print("Missing mandatory parameter: --scm")
+        sys.exit(1)
+    if token == None:
+        print("Missing mandatory parameter: --token")
+        sys.exit(1)
+    if userName == None:
+        print("Missing mandatory parameter: --userName")
+        sys.exit(1)
+    if project == None:
+        print("Missing mandatory parameter: --project")
+        sys.exit(1)
+
+    return scmUrl,token,userName,project,repository,exclude,excludeRepositories,outputFile,pullRequest,inputCommitDepth,entropy,verbose,exitWithError
 
 #################################################################################################
 #################################################################################################
